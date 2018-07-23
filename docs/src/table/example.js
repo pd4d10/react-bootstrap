e27a92623075742af60ccd64a3bfbaeb3a9f4ea6
Rongjian Zhang <pd4d10@gmail.com>
2018-07-23 20:50:49 +0800

    Add Table

---
+++ docs/src/table/example.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Table } from 'relaunch'
+
+export default class Example extends Component {
+  render() {
+    return (
+      <div>
+        <Table />
+      </div>
+    )
+  }
+}
---
+++ src/table.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import $c from 'classnames'
+import { CommonProps } from './utils'
+
+interface TableProps extends CommonProps {
+  items: any[]
+  renderItem: (item: any) => React.ReactNode
+}
+
+export class Table extends React.Component<TableProps> {
+  render() {
+    const { className, items } = this.props
+    return (
+      <table className={$c(className, 'table')}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+}
