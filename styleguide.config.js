34340b8f8d02aadc7dab9cdbff9fb55094fdd00e
Rongjian Zhang <pd4d10@gmail.com>
2018-11-06 17:37:54 +0800

    build: use styleguidist as doc tool

---
+++ styleguide.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  components: 'src/button/**/*.tsx',
+  propsParser: require('react-docgen-typescript').parse,
+  require: ['bootstrap/dist/css/bootstrap.min.css'], // https://github.com/styleguidist/react-styleguidist/blob/master/docs/Cookbook.md#how-to-add-custom-javascript-and-css-or-polyfills
+  webpackConfig: {
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'awesome-typescript-loader',
+          options: { errorsAsWarnings: true },
+        },
+        { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      ],
+    },
+  },
+}
