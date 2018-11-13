ee9aa19714325d6ca9acdfb27b17c099cb3229a2
Rongjian Zhang <pd4d10@gmail.com>
2018-11-13 17:42:42 +0800

    fix: entry import link path

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,26 @@ export { Alert } from './components/alert'
 export { Badge } from './components/badge'
 export { Breadcrumb } from './components/breadcrumb/breadcrumb'
 export { BreadcrumbItem } from './components/breadcrumb/breadcrumb-item'
+
 export { Button } from './components/button/button'
-export { ButtonGroup, ButtonToolbar } from './components/button/button-group'
-export {
-  Card,
-  CardText,
-  CardTitle,
-  CardBody,
-  CardImage,
-  CardHeader,
-  CardFooter,
-} from './components/card/card'
+export { ButtonGroup } from './components/button/button-group'
+export { ButtonToolbar } from './components/button/button-toolbar'
+
+export { Card } from './components/card/card'
+export { CardText } from './components/card/card-text'
+export { CardTitle } from './components/card/card-title'
+export { CardHeader } from './components/card/card-header'
+export { CardFooter } from './components/card/card-footer'
+export { CardImage } from './components/card/card-image'
+
 export { Carousel } from './components/carousel'
 export { Collapse } from './components/collapse'
-export {
-  Dropdown,
-  DropdownDivider,
-  DropdownMenu,
-  DropdownItem,
-  DropdownHeader,
-} from './components/dropdown/dropdown'
+
+export { Dropdown } from './components/dropdown/dropdown'
+export { DropdownDivider } from './components/dropdown/dropdown-divider'
+export { DropdownMenu } from './components/dropdown/dropdown-menu'
+export { DropdownItem } from './components/dropdown/dropdown-item'
+export { DropdownHeader } from './components/dropdown/dropdown-header'
 
 export { FormGroup } from './components/form/form-group'
 export { Input } from './components/form/input'
@@ -36,7 +36,11 @@ export { Col } from './components/layout/col'
 
 export { Modal } from './components/modal'
 export { Navbar, NavbarNav, NavbarBrand } from './components/navbar'
-export { Nav, NavItem, NavLink } from './components/nav/nav'
+
+export { Nav } from './components/nav/nav'
+export { NavItem } from './components/nav/nav-item'
+export { NavLink } from './components/nav/nav-link'
+
 export { Jumbotron } from './components/jumbotron'
 export { Pagination } from './components/pagination'
 export { Progress } from './components/progress'
