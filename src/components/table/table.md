50a023d915a3d9dc81aab031702aec0dad45939b
Rongjian Zhang <pd4d10@gmail.com>
2018-11-15 13:16:55 +0800

    feat(table): basic implementation

--- src/components/table/table.md
+++ src/components/table/table.md
@@ -1,1 +1,69 @@
 ### Example
+
+```js
+<Table
+  schema={[
+    { title: '#', key: 'index' },
+    { title: 'First', key: 'first' },
+    { title: 'Last', key: 'last' },
+    { title: 'Handle', key: 'handle' },
+  ]}
+  rows={[
+    { index: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
+    { index: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
+    { index: 3, first: 'Larry', last: 'the Bird', handle: '@twitter' },
+  ]}
+/>
+```
+
+### Theme
+
+```js
+<Table
+  theme="dark"
+  schema={[
+    { title: '#', key: 'index' },
+    { title: 'First', key: 'first' },
+    { title: 'Last', key: 'last' },
+    { title: 'Handle', key: 'handle' },
+  ]}
+  rows={[
+    { index: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
+    { index: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
+    { index: 3, first: 'Larry', last: 'the Bird', handle: '@twitter' },
+  ]}
+/>
+```
+
+### Thead theme
+
+```js
+<Table
+  theadTheme="dark"
+  schema={[
+    { title: '#', key: 'index' },
+    { title: 'First', key: 'first' },
+    { title: 'Last', key: 'last' },
+    { title: 'Handle', key: 'handle' },
+  ]}
+  rows={[
+    { index: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
+    { index: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
+    { index: 3, first: 'Larry', last: 'the Bird', handle: '@twitter' },
+  ]}
+/>
+<Table
+  theadTheme="light"
+  schema={[
+    { title: '#', key: 'index' },
+    { title: 'First', key: 'first' },
+    { title: 'Last', key: 'last' },
+    { title: 'Handle', key: 'handle' },
+  ]}
+  rows={[
+    { index: 1, first: 'Mark', last: 'Otto', handle: '@mdo' },
+    { index: 2, first: 'Jacob', last: 'Thornton', handle: '@fat' },
+    { index: 3, first: 'Larry', last: 'the Bird', handle: '@twitter' },
+  ]}
+/>
+```
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -2,35 +2,43 @@ import * as React from 'react'
 import * as types from '../../types'
 import { $c } from '../../utils'
 
-type TableProps = types.CommonProps<'table'> & {
-  items: any[]
-  renderItem: (item: any) => React.ReactNode
+export interface TableProps<T> extends types.CommonProps<'table'> {
+  theme?: 'light' | 'dark'
+  theadTheme?: 'light' | 'dark'
+  striped?: boolean
+  schema: {
+    title: string
+    key: string
+    render: (item: T, index: number) => React.ReactElement<any>
+  }[]
+  rows: T[]
 }
 
-export class Table extends React.Component<TableProps> {
+export class Table<T> extends React.Component<TableProps<T>> {
   render() {
-    const { items, render, ...rest } = this.props
-    rest.className = $c(rest.className, 'table')
-    if (render) {
-      return render({ className: rest.className })
-    }
+    const { theme, theadTheme, schema, rows, ...rest } = this.props
+    rest.className = $c(rest.className, 'table', theme && `table-${theme}`)
     return (
       <table {...rest}>
-        <thead>
+        <thead className={$c(theadTheme && `thead-${theadTheme}`)}>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            {schema.map(({ title, key }) => (
+              <th key={key}>{title}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {items.map(item => (
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+          {rows.map((item, index) => (
+            <tr key={index}>
+              {schema.map(({ key, render }) => {
+                if (render) {
+                  return <td>{render(item, index)}</td>
+                } else if (typeof item[key] !== 'undefined') {
+                  return <td>{item[key]}</td>
+                } else {
+                  throw new Error('')
+                }
+              })}
             </tr>
           ))}
         </tbody>
